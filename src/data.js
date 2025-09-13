d1706800cdf123331394783ac1c89c59b1fdeea7
Mukola08 <163895904+Mukola08@users.noreply.github.com>
2025-09-13 18:53:07 +0300

    fuul

--- src/data.js
+++ src/data.js
@@ -2,4 +2,4 @@ import phone from "./phone.json";
 import template from "./phone-template.hbs";
 
 const list = document.querySelector(".list_js");
-list.innerHTML = template(phone);
+list.innerHTML = template({ phone });
